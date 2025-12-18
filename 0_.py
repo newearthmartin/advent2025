38dcb13f4e94cafde85a1be23185bdf883476f8d
Martin Massera <martinmassera@gmail.com>
2025-12-18 13:14:40 -0300

    generic day

---
+++ 0_.py
@@ -0,0 +1,15 @@
+with open('0?.txt') as f:
+    lines = (line for l in f.readlines() if (line := l.strip()))
+
+
+def part1():
+    return 0
+
+
+def part2():
+    return 0
+
+
+print(part1())
+print(part2())
+
